2e3a97f9030f777015a40473a2db2a20c285dbc6
dute0987 <dute0987@gmail.com>
2022-03-20 15:50:34 +0530

    Vishal Dute -Main file for Email Address

---
+++ Detail.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistrationUsingLambda
+{
+    public class Detail
+    {
+        public string FirstName;
+        public string LastName;
+        public string Email;
+
+        public Detail(string firstname, string lastName, string email)
+        {
+            FirstName = firstname;
+            LastName = lastName;
+            Email = email;
+        }
+    }
+}
