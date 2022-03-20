0114422d8a47db08ec978362b6e3c9edf918cab7
dute0987 <dute0987@gmail.com>
2022-03-20 15:38:37 +0530

    Vishal Dute - Main file for Last Name

---
+++ Detail.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistrationUsingLambda
+{
+    public class Detail
+    {
+        public string FirstName;
+        public string LastName;
+
+        public Detail(string firstname, string lastName)
+        {
+            FirstName = firstname;
+            LastName = lastName;
+        }
+    }
+}
