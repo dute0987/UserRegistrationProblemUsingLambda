85974168321dba97778a96b55f0557bb2f5eba95
dute0987 <dute0987@gmail.com>
2022-03-20 15:55:29 +0530

    Vishal Dute -Program.cs file for Mobile Format

--- Program.cs
+++ Program.cs
@@ -5,17 +5,19 @@ namespace UserRegistrationUsingLambda
     {
         public static void Main(string[]args)
         {
-	    Console.WriteLine("Welcome to User registration Problem Using Lambda")
             List<Detail> detaillist = new List<Detail>();
             AddRecords(detaillist);
             
         }
         private static void AddRecords(List<Detail> detaillist)
         {
-            detaillist.Add(new Detail("Vishal"));
+            detaillist.Add(new Detail("Vishal","Dute","dute0987@gmail.com","+91 8978776889"));
             foreach (Detail detail in detaillist)
             {
-                Console.WriteLine("First Name: " + detail.FirstName);
+                //Console.WriteLine("First Name: " + detail.FirstName);
+                //Console.WriteLine("Last name : " + detail.LastName);
+                //Console.WriteLine("Email ID :"+ detail.Email);
+                Console.WriteLine("Phone Number : " + detail.PhoneNumber);
             }
         }
     }
