c846b4f31e7410a6a09defc60e594d59d1eab288
dute0987 <dute0987@gmail.com>
2022-03-20 15:29:07 +0530

    Vishal Dute- Greeting message for User Registration Problem Using Lambda

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace UserRegistrationUsingLambda
+{
+    class Program
+    {
+        public static void Main(string[]args)
+        {
+	    Console.WriteLine("Welcome to User registration Problem Using Lambda")
+            List<Detail> detaillist = new List<Detail>();
+            AddRecords(detaillist);
+            
+        }
+        private static void AddRecords(List<Detail> detaillist)
+        {
+            detaillist.Add(new Detail("Vishal"));
+            foreach (Detail detail in detaillist)
+            {
+                Console.WriteLine("First Name: " + detail.FirstName);
+            }
+        }
+    }
+}
