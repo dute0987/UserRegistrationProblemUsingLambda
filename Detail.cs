e81785c86137fcf73902e04b03bb79434f4e2273
dute0987 <dute0987@gmail.com>
2022-03-20 16:01:24 +0530

    Vishal Dute -Main file for Passward Program

---
+++ Detail.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistrationUsingLambda
+{
+    public class Detail
+    {
+        public string FirstName;
+        public string LastName;
+        public string Email;
+        public string PhoneNumber;
+        public string Passward;
+
+        public Detail(string firstname, string lastName, string email, string phoneNumber, string passward)
+        {
+            FirstName = firstname;
+            LastName = lastName;
+            Email = email;
+            PhoneNumber = phoneNumber;
+            Passward = passward;
+        }
+    }
+}
