216c96b6bac84a5450f15d2839c1ebd6cc130b82
dute0987 <dute0987@gmail.com>
2022-03-20 15:56:12 +0530

    Vishal Dute -Main file for Mobile Format

---
+++ Detail.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistrationUsingLambda
+{
+    public class Detail
+    {
+        public string FirstName;
+        public string LastName;
+        public string Email;
+        public string PhoneNumber;
+
+        public Detail(string firstname, string lastName, string email, string phoneNumber)
+        {
+            FirstName = firstname;
+            LastName = lastName;
+            Email = email;
+            PhoneNumber = phoneNumber;
+        }
+    }
+}
